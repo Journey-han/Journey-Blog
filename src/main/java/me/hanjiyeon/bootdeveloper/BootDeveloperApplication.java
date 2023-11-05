bf88ed6960940785bdec542f85ad1437240b0774
Journey-han <jjyy0602@gmail.com>
2023-11-05 22:30:48 +0900

    Initial commit

---
+++ src/main/java/me/hanjiyeon/bootdeveloper/BootDeveloperApplication.java
@@ -0,0 +1,14 @@
+package me.hanjiyeon.bootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDeveloperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootDeveloperApplication.class, args);
+    }
+
+}
+ 
---
+++ src/test/java/me/hanjiyeon/bootdeveloper/BootDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hanjiyeon.bootdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDeveloperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
